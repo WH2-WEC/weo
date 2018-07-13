2fadc23af5028f3dbdf9b5aab99a68b71c0d8499
DrunkFlamingo <matte901@gmail.com>
2018-07-13 11:43:33 -0400

    added factions at war with

--- geopolitics/export_helpers__geopolitical_system.lua
+++ geopolitics/export_helpers__geopolitical_system.lua
@@ -19,6 +19,27 @@ core:add_listener(
     true)
 
 
+core:add_listener(
+    "GeopoliticsRegionOwnershipChangeOccupied",
+    "GarrisonOccupiedEvent",
+    true,
+    function(context)
+        local war_list = context:character():faction():factions_at_war_with() --:CA_FACTION_LIST
+        local conquering_faction = context:character():faction() --:CA_FACTION
+        gpm:get_faction(conquering_faction:name()):set_region_changed()
+        for i = 0, war_list:num_items() do
+            local current_faction_key = war_list:item_at(i):name()
+            gpm:get_faction(current_faction_key):set_region_changed()
+        end
+    end,
+    true)
 
-
-
+core:add_listener(
+    "GeopoliticsRegionOwnershipChangeConfederation",
+    "FactionJoinsConfederation",
+    true,
+    function(context)
+        local confed_name = context:confederation():name() --:string
+        gpm:get_faction(confed_name):set_region_changed()
+    end,
+    true)
