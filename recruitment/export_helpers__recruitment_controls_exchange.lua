533b056f7290b94f1066e20dbaad1c1672476382
DrunkFlamingo <matte901@gmail.com>
2018-07-27 13:17:49 -0400

    refactored exchange script completely

--- recruitment/export_helpers__recruitment_controls_exchange.lua
+++ recruitment/export_helpers__recruitment_controls_exchange.lua
@@ -1,16 +1,7 @@
 
-local first_army_count = {} --:map<string, number>
-local first_army_index = {} --:vector<string>
-local first_army_group_count = {} --:map<string, number>
-local second_army_index = {} --:vector<string>
-local second_army_count = {} --:map<string, number>
-local second_army_group_count = {} --:map<string, number>
-local first_army_selected = {} --:map<number, boolean>
-local second_army_selected = {} --:map<number, boolean>
-
 
 rm:error_checker()
---v function(panel: string, index: number) --> string
+--v function(panel: string, index: number) --> (string, boolean)
 local function GetUnitNameInExchange(panel, index)
     local Panel = find_uicomponent(core:get_ui_root(), "unit_exchange", panel)
     if not not Panel then
@@ -23,76 +14,126 @@ local function GetUnitNameInExchange(panel, index)
             local infoend = string.find(rawstring, "]]") - 1;
             local armyUnitName = string.sub(rawstring, infostart, infoend)
             rm:log("Found unit ["..armyUnitName.."] at ["..index.."] ")
-            return armyUnitName
+            local is_transfered = false --:boolean
+            local transferArrow = find_uicomponent(armyUnit, "exchange_arrow")
+            if not not transferArrow then 
+                is_transfered = transferArrow:Visible()
+            end
+            return armyUnitName, is_transfered
         else
-            return nil
+            return nil, false
         end
     end
-    return nil
+    return nil, false
 end
 
---v function(panel: string, index: number)
-local function lock_unit_from_transfer(panel, index)
-    local armyUnit = find_uicomponent(core:get_ui_root(), "unit_exchange", panel, "units", "UnitCard" .. index);
-    if not not armyUnit then
-        armyUnit:SetInteractive(false)
-        --[[
-        if Util then
-            if not Util.getComponentWithName("cui_profile_marker_"..panel.."_"..index) then
-                local ProfileIcon = Image.new("cui_profile_marker_"..panel.."_"..index, armyUnit, "ui/custom/recruitment_controls/locked_unit.png")
-                ProfileIcon:PositionRelativeTo(armyUnit, 20, 30)
-                ProfileIcon:GetContentComponent():SetTooltipText("The other army is at their limit for this unit!")
-                ProfileIcon:Resize(50, 60)
+--v function(reason: string)
+local function LockExchangeButton(reason)
+    local ok_button = find_uicomponent(core:get_ui_root(), "unit_exchange", "hud_center_docker", "ok_cancel_buttongroup", "button_ok")
+    if not not ok_button then
+        ok_button:SetInteractive(false)
+        ok_button:SetImage("ui/skins/default/icon_disband.png")
+    else
+        rm:log("ERROR: could not find the exchange ok button!")
+    end
+end
+
+--v function()
+local function UnlockExchangeButton()
+    local ok_button = find_uicomponent(core:get_ui_root(), "unit_exchange", "hud_center_docker", "ok_cancel_buttongroup", "button_ok")
+    if not not ok_button then
+        ok_button:SetInteractive(true)
+        ok_button:SetImage("ui/skins/default/icon_check.png")
+    else
+        rm:log("ERROR: could not find the exchange ok button!")
+    end
+end
+
+--v function(first_army_count: map<string, number>, second_army_count:map<string, number>) --> (boolean, string)
+local function are_armies_valid(first_army_count, second_army_count)
+
+    for unitID, count in pairs(first_army_count) do
+        if count > rm:get_quantity_limit_for_unit(unitID) then
+            return false, "Too many individual restricted units in an army!"
+        end
+        local groups = rm:get_groups_for_unit(unitID)
+        for i = 1, #groups do
+            local grouped_units = rm:get_units_in_group(groups[i])
+            local group_total = 0 --:number
+            for j = 1, #grouped_units do
+                if first_army_count[grouped_units[j]] == nil then
+                    first_army_count[grouped_units[j]] = 0
+                end
+                group_total = group_total + (first_army_count[grouped_units[j]] * rm:get_weight_for_unit(grouped_units[j]))
+            end
+            if group_total > rm:get_quantity_limit_for_group(groups[i]) then
+                return false, "Too many units from group "..rm:get_ui_name_for_group(groups[i]).." in an army!"
+            end
+        end
+    end
+
+    for unitID, count in pairs(second_army_count) do
+        if count > rm:get_quantity_limit_for_unit(unitID) then
+            return false, "Too many individual restricted units in an army!"
+        end
+        local groups = rm:get_groups_for_unit(unitID)
+        for i = 1, #groups do
+            local grouped_units = rm:get_units_in_group(groups[i])
+            local group_total = 0 --:number
+            for j = 1, #grouped_units do
+                if second_army_count[grouped_units[j]] == nil then
+                    second_army_count[grouped_units[j]] = 0
+                end
+                group_total = group_total + (second_army_count[grouped_units[j]] * rm:get_weight_for_unit(grouped_units[j]))
+            end
+            if group_total > rm:get_quantity_limit_for_group(groups[i]) then
+                return false, "Too many units from group "..rm:get_ui_name_for_group(groups[i]).." in an army!"
+            end
+        end
+    end
+    return true, "valid"
+end
+
+--v function() --> (map<string, number>, map<string, number>)
+local function count_armies()
+    local first_army_count = {} --:map<string, number>
+    local second_army_count = {} --:map<string, number>
+
+    for i = 1, 20 do
+        local unitID, is_transfer = GetUnitNameInExchange("main_units_panel_1", i)
+        if not not unitID then
+            if is_transfer then
+                if second_army_count[unitID] == nil then
+                    second_army_count[unitID] = 0
+                end
+                second_army_count[unitID] = second_army_count[unitID] + 1
             else
-                local ProfileIcon = Util.getComponentWithName("cui_profile_marker_"..panel.."_"..index) --# assume ProfileIcon: IMAGE
-                ProfileIcon:SetImage("ui/custom/recruitment_controls/locked_unit.png")
-                ProfileIcon:GetContentComponent():SetTooltipText("The other army is at their limit for this unit!")
-                ProfileIcon:Resize(50, 60)
+                if first_army_count[unitID] == nil then
+                    first_army_count[unitID] = 0
+                end
+                first_army_count[unitID] = first_army_count[unitID] + 1
             end
-            
-            
-            if not not Util.getComponentWithName("cui_profile_marker_"..panel.."_"..index) then
-                local ProfileIcon = Util.getComponentWithName("cui_profile_marker_"..panel.."_"..index) --# assume ProfileIcon: IMAGE
-                ProfileIcon:GetContentComponent():SetTooltipText("The other army is at their limit for this unit!")
+        end
+    end
+
+    for i = 1, 20 do
+        local unitID, is_transfer = GetUnitNameInExchange("main_units_panel_2", i)
+        if not not unitID then
+            if not is_transfer then
+                if second_army_count[unitID] == nil then
+                    second_army_count[unitID] = 0
+                end
+                second_army_count[unitID] = second_army_count[unitID] + 1
+            else
+                if first_army_count[unitID] == nil then
+                    first_army_count[unitID] = 0
+                end
+                first_army_count[unitID] = first_army_count[unitID] + 1
             end
-        else
-            rm:log("UIMF is not installed!")
-        end
-        --]]
-    end
-end
-
---v function(panel: string, index: number)
-local function unlock_unit_from_transfer(panel, index)
-    local armyUnit = find_uicomponent(core:get_ui_root(), "unit_exchange", panel, "units", "UnitCard" .. index);
-    if not not armyUnit then
-        armyUnit:SetInteractive(true)
-    end
-end
-
-
---v function(unitID: string, count_of_other_army: map<string, number>, group_count_of_other_army:map<string, number>) --> boolean
-local function should_restrict_unit_from_transfer(unitID, count_of_other_army, group_count_of_other_army)
-    rm:log("Checking transfer restrictions for unit ["..unitID.."] ")
-    local groups = rm:get_groups_for_unit(unitID)
-    for j = 1, #groups do
-        if group_count_of_other_army[groups[j]] == nil then
-            group_count_of_other_army[groups[j]] = 0
-        end
-        if group_count_of_other_army[groups[j]] + rm:get_weight_for_unit(unitID) - 1 >= rm:get_quantity_limit_for_group(groups[j]) then
-            rm:log("Should lock unit ["..unitID.."] from being transferred to the other army!")
-            return true
         end
     end
-    if count_of_other_army[unitID] == nil then
-        count_of_other_army[unitID] = 0 
-    end
-    if count_of_other_army[unitID] >= rm:get_quantity_limit_for_unit(unitID) then
-        rm:log("Should lock unit ["..unitID.."] from being transferred to the other army!")
-        return true
-    end
-    rm:log("Should NOT lock unit ["..unitID.."] from being transferred to the other army!")
-    return false
+
+    return first_army_count, second_army_count
 end
 
 
@@ -100,7 +141,7 @@ end
 
 
 core:add_listener(
-    "RecruiterManagerOnRecruitPanelOpened",
+    "RecruiterManagerOnExchangePanelOpened",
     "PanelOpenedCampaign",
     function(context) 
         return context.string == "unit_exchange"; 
@@ -109,120 +150,53 @@ core:add_listener(
         cm:callback(function() --do this on a delay so the panel has time to fully open before the script tries to read it!
            -- print_all_uicomponent_children(find_uicomponent(core:get_ui_root(), "unit_exchange"))
 
-           second_army_index = {} 
-           second_army_count = {} 
-           second_army_group_count = {}
-           first_army_count = {}
-           first_army_index = {}
-           first_army_group_count = {} 
-           first_army_selected = {}
-           second_army_selected = {}
-            for i = 1, 20 do
-                local unitID = GetUnitNameInExchange("main_units_panel_1", i)
-                if is_string(unitID) then
-                    if first_army_count[unitID] == nil then 
-                        first_army_count[unitID] = 0
-                    end
-                    first_army_count[unitID] = first_army_count[unitID] + 1
-                    local groups = rm:get_groups_for_unit(unitID)
-                    for k = 1, #groups do
-                        if first_army_group_count[groups[k]] == nil then 
-                            first_army_group_count[groups[k]] = 0
-                        end
-                        first_army_group_count[groups[k]] = first_army_group_count[groups[k]] + (1 *rm:get_weight_for_unit(unitID) )
-                    end
-                    table.insert(first_army_index, unitID)
-                    rm:log("added unit ["..unitID.."] to a count! ")
-                    if rm:unit_has_ui_profile(unitID) then
-                        
-                        local unit_profile = rm:get_ui_profile_for_unit(unitID)
-                        --[[
-                        if Util then 
-                            if not not Util.getComponentWithName("cui_profile_marker_main_units_panel_1_"..i) then
-                                Util.getComponentWithName("cui_profile_marker_main_units_panel_1_"..i):Delete()
-                            end
-                            if not Util.getComponentWithName("cui_profile_marker_main_units_panel_1_"..i) then
-                                local iconParent = find_uicomponent(core:get_ui_root(), "unit_exchange", "main_units_panel_1", "units", "UnitCard"..i)
-                                local ProfileIcon = Image.new("cui_profile_marker_main_units_panel_1_"..i, iconParent, unit_profile._image)
-                                ProfileIcon:PositionRelativeTo(iconParent, 20, 30)
-                                ProfileIcon:GetContentComponent():SetTooltipText(unit_profile._text)
-                            end
-                        else
-                            rm:log("UIMF not installed!")
-                        end
-                        --]]
-                    else
-                        rm:log("Unit has no profile!")
-                    end
-                else
-                    rm:log("Could not find a unit at ["..i.."] in an exchanging army, aborting the loop!")
-                    break
-                end
+            local first_army, second_army = count_armies()
+            local valid_armies, reason = are_armies_valid(first_army, second_army)
+            if valid_armies then
+                UnlockExchangeButton()
+            else
+                rm:log("locking exchange button for reason ["..reason.."] ")
+                LockExchangeButton(reason)
             end
-
-            for i = 1, 20 do
-                local unitID = GetUnitNameInExchange("main_units_panel_2", i)
-                if is_string(unitID) then
-                    if second_army_count[unitID] == nil then 
-                        second_army_count[unitID] = 0
-                    end
-                    second_army_count[unitID] = second_army_count[unitID] + 1
-                    local groups = rm:get_groups_for_unit(unitID)
-                    for k = 1, #groups do
-                        if second_army_group_count[groups[k]] == nil then 
-                            second_army_group_count[groups[k]] = 0
-                        end
-                        second_army_group_count[groups[k]] = second_army_group_count[groups[k]] + (1 *rm:get_weight_for_unit(unitID) )
-                    end
-                    table.insert(second_army_index, unitID)
-                    rm:log("added unit ["..unitID.."] to a count! ")
-                    if rm:unit_has_ui_profile(unitID) then
-                        
-                        local unit_profile = rm:get_ui_profile_for_unit(unitID)
-                        --[[
-                        if Util then 
-                            if not not Util.getComponentWithName("cui_profile_marker_main_units_panel_2_"..i) then
-                                Util.getComponentWithName("cui_profile_marker_main_units_panel_2_"..i):Delete()
-                            end
-                            if not Util.getComponentWithName("cui_profile_marker_main_units_panel_2_"..i) then
-                                local iconParent = find_uicomponent(core:get_ui_root(), "unit_exchange", "main_units_panel_2", "units", "UnitCard"..i)
-                                local ProfileIcon = Image.new("cui_profile_marker_main_units_panel_2_"..i, iconParent, unit_profile._image)
-                                ProfileIcon:PositionRelativeTo(iconParent, 20, 30)
-                                ProfileIcon:GetContentComponent():SetTooltipText(unit_profile._text)
-                            elseif not not Util.getComponentWithName("cui_profile_marker_main_units_panel_2_"..i) then
-                                local ProfileIcon = Util.getComponentWithName("cui_profile_marker_main_units_panel_2_"..i) --# assume ProfileIcon: IMAGE
-                                ProfileIcon:GetContentComponent():SetTooltipText(unit_profile._text)
-                                ProfileIcon:SetImage(unit_profile._image)
-                            end
-                        else
-                            rm:log("UIMF not installed!")
-                        end
-                        --]]
-                    else
-                        rm:log("Unit has no profile!")
-                    end
-                else
-                    rm:log("Could not find a unit at ["..i.."] in an exchanging army, aborting the loop!")
-                    break
-                end
-            end
-
-            for i = 1, #first_army_index do
-                if should_restrict_unit_from_transfer(first_army_index[i], second_army_count, second_army_group_count) then
-                    lock_unit_from_transfer("main_units_panel_1", i)
-                else
-                    unlock_unit_from_transfer("main_units_panel_1", i)
-                end
-            end
-            for i = 1, #second_army_index do
-                if should_restrict_unit_from_transfer(second_army_index[i], first_army_count, first_army_group_count) then
-                    lock_unit_from_transfer("main_units_panel_2", i)
-                else
-                    unlock_unit_from_transfer("main_units_panel_2", i)
-                end
-            end
-
         end, 0.1)
+    end,
+    true
+)
+
+
+core:add_listener(
+    "RecruiterManagerOnExchangeOptionClicked",
+    "ComponentLClickUp",
+    function(context)
+       return not not string.find(context.string, "UnitCard") 
+    end,
+    function(context)
+       rm:log("refreshing army validity")
+       cm:callback( function()
+            local first_army, second_army = count_armies()
+            local valid_armies, reason = are_armies_valid(first_army, second_army)
+            if valid_armies then
+                UnlockExchangeButton()
+            else
+                rm:log("locking exchange button for reason ["..reason.."] ")
+                LockExchangeButton(reason)
+            end
+        end, 0.1)
+    end,
+    true);
+
+
+core:add_listener(
+    "RecruiterManagerOnExchangePanelClosed",
+    "PanelClosedCampaign",
+    function(context)
+        return context.string == "unit_exchange"
+    end,
+    function(context)
+        rm:log("Exchange panel closed, setting armies stale!")
+        for cqi, character in pairs(rm:characters()) do
+            character:set_army_stale()
+        end
     end,
     true
 )
