8d513fc97f1ba3fcf3232149652ec64ec88d6214
DrunkFlamingo <matte901@gmail.com>
2018-07-27 09:38:23 -0400

    rolled back some error checking changes

--- recruitment/RecruitmentControls.lua
+++ recruitment/RecruitmentControls.lua
@@ -99,71 +99,137 @@ end
 --v [NO_CHECK] 
 --v function (self: RECRUITER_MANAGER)
 function recruiter_manager.error_checker(self)
-    --Vanish's PCaller
+  --Vanish's PCaller
     --All credits to vanish
-    --safely call a function
     --v function(func: function) --> any
-    function safeCall(func)
-        local status, result = pcall(func)
-        if not status then
-            self:log("********ERROR DETECTED***********")
-            self:log(tostring(result))
-            self:log(debug.traceback());
-            
-        end
-        return result;
-    end
-    --pack args
-    --v [NO_CHECK] function(...: any)
-    function pack2(...) return {n=select('#', ...), ...} end
-    --unpack args
-    --v [NO_CHECK] function(t: vector<WHATEVER>) --> vector<WHATEVER>
-    function unpack2(t) return unpack(t, 1, t.n) end
-    
-    --wrap a function in safecalls
-    --v [NO_CHECK] function(f: function(), argProcessor: function()) --> function()
-    function wrapFunction(f, argProcessor)
-        return function(...)
-            --output("start wrap ");
-            local someArguments = pack2(...);
-            if argProcessor then
-                safeCall(function() argProcessor(someArguments) end)
+        function safeCall(func)
+            --output("safeCall start");
+            local status, result = pcall(func)
+            if not status then
+                SFOLOG(tostring(result), "ERROR CHECKER")
+                SFOLOG(debug.traceback(), "ERROR CHECKER");
             end
-            local result = pack2(safeCall(function() return f(unpack2( someArguments )) end));
-            --for k, v in pairs(result) do
-            --    output("Result: " .. tostring(k) .. " value: " .. tostring(v));
-            --end
-            --output("end wrap ");
-            return unpack2(result);
-            end
-    end
-    --wrap all callbacks
-    --[[ --untested
-    local currentCallBack = cm.callback;
-    --v [NO_CHECK] function(cm: any, callback: function(), timer: any, name: any)
-    function myCallBack(cm, callback, timer, name)
-        currentCallback(cm, wrapFunction(callback), timer, name)
-    end
-    cm.callback = MyCallBack;
-    --]]
-    --wrap all listeners
-    local currentAddListener = core.add_listener;
-    --v [NO_CHECK] function(core: any, listenerName: any, eventName: any, conditionFunc: any, listenerFunc: any, persistent: any)
-    function myAddListener(core, listenerName, eventName, conditionFunc, listenerFunc, persistent)
-        local wrappedCondition = nil;
-        if is_function(conditionFunc) then
-            --wrappedCondition =  wrapFunction(conditionFunc, function(arg) output("Callback condition called: " .. listenerName .. ", for event: " .. eventName); end);
-            wrappedCondition =  wrapFunction(conditionFunc);
-        else
-            wrappedCondition = conditionFunc;
+            --output("safeCall end");
+            return result;
         end
-        currentAddListener(
-            core, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc), persistent
-            --core, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc, function(arg) output("Callback called: " .. listenerName .. ", for event: " .. eventName); end), persistent
+        
+        --local oldTriggerEvent = core.trigger_event;
+        
+        --v [NO_CHECK] function(...: any)
+        function pack2(...) return {n=select('#', ...), ...} end
+        --v [NO_CHECK] function(t: vector<WHATEVER>) --> vector<WHATEVER>
+        function unpack2(t) return unpack(t, 1, t.n) end
+        
+        --v [NO_CHECK] function(f: function(), argProcessor: function()) --> function()
+        function wrapFunction(f, argProcessor)
+            return function(...)
+                --output("start wrap ");
+                local someArguments = pack2(...);
+                if argProcessor then
+                    safeCall(function() argProcessor(someArguments) end)
+                end
+                local result = pack2(safeCall(function() return f(unpack2( someArguments )) end));
+                --for k, v in pairs(result) do
+                --    output("Result: " .. tostring(k) .. " value: " .. tostring(v));
+                --end
+                --output("end wrap ");
+                return unpack2(result);
+                end
+        end
+        
+        -- function myTriggerEvent(event, ...)
+        --     local someArguments = { ... }
+        --     safeCall(function() oldTriggerEvent(event, unpack( someArguments )) end);
+        -- end
+        
+        --v [NO_CHECK] function(fileName: string)
+        function tryRequire(fileName)
+            local loaded_file = loadfile(fileName);
+            if not loaded_file then
+                output("Failed to find mod file with name " .. fileName)
+            else
+                output("Found mod file with name " .. fileName)
+                output("Load start")
+                local local_env = getfenv(1);
+                setfenv(loaded_file, local_env);
+                loaded_file();
+                output("Load end")
+            end
+        end
+        
+        --v [NO_CHECK] function(f: function(), name: string)
+        function logFunctionCall(f, name)
+            return function(...)
+                output("function called: " .. name);
+                return f(...);
+            end
+        end
+        
+        --v [NO_CHECK] function(object: any)
+        function logAllObjectCalls(object)
+            local metatable = getmetatable(object);
+            for name,f in pairs(getmetatable(object)) do
+                if is_function(f) then
+                    output("Found " .. name);
+                    if name == "Id" or name == "Parent" or name == "Find" or name == "Position" or name == "CurrentState"  or name == "Visible"  or name == "Priority" or "Bounds" then
+                        --Skip
+                    else
+                        metatable[name] = logFunctionCall(f, name);
+                    end
+                end
+                if name == "__index" and not is_function(f) then
+                    for indexname,indexf in pairs(f) do
+                        output("Found in index " .. indexname);
+                        if is_function(indexf) then
+                            f[indexname] = logFunctionCall(indexf, indexname);
+                        end
+                    end
+                    output("Index end");
+                end
+            end
+        end
+        
+        -- logAllObjectCalls(core);
+        -- logAllObjectCalls(cm);
+        -- logAllObjectCalls(game_interface);
+        
+        core.trigger_event = wrapFunction(
+            core.trigger_event,
+            function(ab)
+                --output("trigger_event")
+                --for i, v in pairs(ab) do
+                --    output("i: " .. tostring(i) .. " v: " .. tostring(v))
+                --end
+                --output("Trigger event: " .. ab[1])
+            end
+        );
+        
+        cm.check_callbacks = wrapFunction(
+            cm.check_callbacks,
+            function(ab)
+                --output("check_callbacks")
+                --for i, v in pairs(ab) do
+                --    output("i: " .. tostring(i) .. " v: " .. tostring(v))
+                --end
+            end
         )
-    end
-    core.add_listener = myAddListener;
-
+        
+        local currentAddListener = core.add_listener;
+        --v [NO_CHECK] function(core: any, listenerName: any, eventName: any, conditionFunc: any, listenerFunc: any, persistent: any)
+        function myAddListener(core, listenerName, eventName, conditionFunc, listenerFunc, persistent)
+            local wrappedCondition = nil;
+            if is_function(conditionFunc) then
+                --wrappedCondition =  wrapFunction(conditionFunc, function(arg) output("Callback condition called: " .. listenerName .. ", for event: " .. eventName); end);
+                wrappedCondition =  wrapFunction(conditionFunc);
+            else
+                wrappedCondition = conditionFunc;
+            end
+            currentAddListener(
+                core, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc), persistent
+                --core, listenerName, eventName, wrappedCondition, wrapFunction(listenerFunc, function(arg) output("Callback called: " .. listenerName .. ", for event: " .. eventName); end), persistent
+            )
+        end
+        core.add_listener = myAddListener;
 end
 
 --group ui names--
