1422f6f1d8eb1d84e9a643def9c81c331d293812
DFlunchbreak <matte901@gmail.com>
2018-07-31 16:50:06 -0400

    attempt one at refactoring the AI functions

--- recruitment/export_helpers__recruitment_controls_ai.lua
+++ recruitment/export_helpers__recruitment_controls_ai.lua
@@ -1,3 +1,35 @@
+local subculture_default_units = {
+    ["wh_dlc03_sc_bst_beastmen"] = "wh_dlc03_bst_inf_gor_herd_0",
+    ["wh_dlc05_sc_wef_wood_elves"] = "wh_dlc05_wef_inf_eternal_guard_1",
+    ["wh_main_sc_brt_bretonnia"] = "wh_main_brt_cav_knights_of_the_realm",
+    ["wh_main_sc_chs_chaos"] = "wh_main_chs_inf_chaos_warriors_0",
+    ["wh_main_sc_dwf_dwarfs"] = "wh_main_dwf_inf_longbeards",
+    ["wh_main_sc_emp_empire"] = "wh_main_emp_inf_halberdiers",
+    ["wh_main_sc_grn_greenskins"] = "wh_main_grn_inf_orc_big_uns",
+    ["wh_main_sc_grn_savage_orcs"] = "wh_main_grn_inf_savage_orc_big_uns",
+    ["wh_main_sc_ksl_kislev"] = "wh_main_emp_inf_halberdiers",
+    ["wh_main_sc_nor_norsca"] = "wh_main_nor_inf_chaos_marauders_0",
+    ["wh_main_sc_teb_teb"] = "wh_main_emp_inf_halberdiers",
+    ["wh_main_sc_vmp_vampire_counts"] = "wh_main_vmp_inf_crypt_ghouls",
+    ["wh2_dlc09_sc_tmb_tomb_kings"] = "wh2_dlc09_tmb_inf_nehekhara_warriors_0",
+    ["wh2_main_sc_def_dark_elves"] = "wh2_main_def_inf_black_ark_corsairs_0",
+    ["wh2_main_sc_hef_high_elves"] = "wh2_main_hef_inf_spearmen_0",
+    ["wh2_main_sc_lzd_lizardmen"] = "wh2_main_lzd_inf_saurus_warriors_1",
+    ["wh2_main_sc_skv_skaven"]  = "wh2_main_skv_inf_stormvermin_0"
+}--:map<string, string>
+
+
+
+
+
+
+
+
+
+
+
+
+
 --ai systems to enforce a proxy of recruitment controls on the AI
 
 --v function(unit_totals: map<string, number>, unitID: string)
@@ -16,7 +48,51 @@ local function increment_group_total(group_totals, groupID, weight)
     group_totals[groupID] = group_totals[groupID] + (1* weight)
 end
 
+--v function(character: CA_CHAR, groupID: string, difference: number)
+local function limit_character(character, groupID, difference)
 
+    local unit_list = character:military_force():unit_list()
+    for j = 0, unit_list:num_items() - 1 do
+        local unit = unit_list:item_at(j):unit_key()
+        local groups_list = rm:get_groups_for_unit(unit)
+        for k = 1, #groups_list do
+            if groups_list[k] == groupID then
+                cm:remove_unit_from_character(cm:char_lookup_str(character:cqi()), unit)
+                cm:grant_unit_to_character(character:cqi(), subculture_default_units[character:faction():subculture()])
+                if rm:get_weight_for_unit(unit) >= difference then
+                    return
+                end
+            end
+        end
+    end
+end
+
+
+
+
+
+--v function(character: CA_CHAR)
+local function rm_ai_character(character)
+
+    if cm:char_is_mobile_general_with_army(character) then
+        local unit_list = character:military_force():unit_list()
+        local unit_totals = {} --:map<string, number>
+        local group_totals = {} --:map<string, number>
+        for j = 0, unit_list:num_items() - 1 do
+            local unit = unit_list:item_at(j):unit_key()
+            local groups_list = rm:get_groups_for_unit(unit)
+            for k = 1, #groups_list do
+                increment_group_total(group_totals, groups_list[k], rm:get_weight_for_unit(unit))
+            end
+        end
+        for groupID, quantity in pairs(group_totals) do
+            local limit = rm:get_quantity_limit_for_group(groupID)
+            if quantity > limit then
+                limit_character(character, groupID, quantity - limit)
+            end
+        end
+    end
+end
 
 
 --v function(faction:CA_FACTION)
@@ -29,50 +105,7 @@ local function rm_ai_evaluation(faction)
     local character_list = faction:character_list()
     for i = 0, character_list:num_items() - 1 do
         local character = character_list:item_at(i)
-        if cm:char_is_mobile_general_with_army(character) then
-            local unit_list = character:military_force():unit_list()
-            local unit_totals = {} --:map<string, number>
-            local group_totals = {} --:map<string, number>
-            for j = 0, unit_list:num_items() - 1 do
-                increment_unit_total(unit_totals, unit_list:item_at(j):unit_key())
-                for k = 1, #rm:get_groups_for_unit(unit_list:item_at(j):unit_key()) do
-                    increment_group_total(group_totals, rm:get_groups_for_unit(unit_list:item_at(j):unit_key())[k], rm:get_weight_for_unit(unit_list:item_at(j):unit_key()))
-                end
-            end
-            for unitID, quantity in pairs(unit_totals) do
-                local quantity_difference = quantity - rm:get_quantity_limit_for_unit(unitID) 
-                if quantity_difference == 0 then
-                    rm:log("AI limiting ["..tostring(character:cqi()).."] for unit ["..unitID.."] ")
-                    cm:apply_effect_bundle_to_characters_force("wec_unit_caps_"..unitID.."_limiter", character:cqi(), 0, true)
-                elseif quantity_difference > 0 then 
-                    rm:log("AI limiting ["..tostring(character:cqi()).."] for unit ["..unitID.."] ")
-                    cm:apply_effect_bundle_to_characters_force("wec_unit_caps_"..unitID.."_limiter", character:cqi(), 0, true)
-                else
-                    rm:log("AI unlimiting ["..tostring(character:cqi()).."] for unit ["..unitID.."] ")
-                    if character:military_force():has_effect_bundle("wec_unit_caps_"..unitID.."_limiter") then
-                        cm:remove_effect_bundle_from_characters_force("wec_unit_caps_"..unitID.."_limiter", character:cqi())
-                    end
-                end
-            end
-            for groupID, quantity in pairs(group_totals) do
-                local quantity_difference = quantity - rm:get_quantity_limit_for_group(groupID)
-                if quantity_difference >= 0 then
-                    rm:log("AI limiting ["..tostring(character:cqi()).."] for unit ["..groupID.."] ")
-                    for m = 1, #rm:get_units_in_group(groupID) do
-                        local unitID = rm:get_units_in_group(groupID)[m]
-                        cm:apply_effect_bundle_to_characters_force("wec_unit_caps_"..unitID.."_limiter", character:cqi(), 0, true)
-                    end
-                else 
-                    rm:log("AI unlimiting ["..tostring(character:cqi()).."] for unit ["..groupID.."] ")
-                    for m = 1, #rm:get_units_in_group(groupID) do
-                        local unitID = rm:get_units_in_group(groupID)[m]
-                        if character:military_force():has_effect_bundle("wec_unit_caps_"..unitID.."_limiter") then
-                            cm:remove_effect_bundle_from_characters_force("wec_unit_caps_"..unitID.."_limiter", character:cqi())
-                        end
-                    end
-                end
-            end
-        end
+        rm_ai_character(character)
     end
 end
 
@@ -85,9 +118,7 @@ core:add_listener(
         return not context:faction():is_human()
     end,
     function(context)
-
         rm_ai_evaluation(context:faction())
-
     end,
     true
 )
