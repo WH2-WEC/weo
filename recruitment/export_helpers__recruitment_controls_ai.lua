423359d3f7e3dc89c044e2402fe4afc510e8efd4
DrunkFlamingo <matte901@gmail.com>
2018-08-03 10:59:36 -0400

    balance and maintenance update

--- recruitment/export_helpers__recruitment_controls_ai.lua
+++ recruitment/export_helpers__recruitment_controls_ai.lua
@@ -1,22 +1,22 @@
 local subculture_default_units = {
-    ["wh_dlc03_sc_bst_beastmen"] = "wh_dlc03_bst_inf_gor_herd_0",
-    ["wh_dlc05_sc_wef_wood_elves"] = "wh_dlc05_wef_inf_eternal_guard_1",
-    ["wh_main_sc_brt_bretonnia"] = "wh_main_brt_cav_knights_of_the_realm",
-    ["wh_main_sc_chs_chaos"] = "wh_main_chs_inf_chaos_warriors_0",
-    ["wh_main_sc_dwf_dwarfs"] = "wh_main_dwf_inf_longbeards",
-    ["wh_main_sc_emp_empire"] = "wh_main_emp_inf_swordsmen",
-    ["wh_main_sc_grn_greenskins"] = "wh_main_grn_inf_orc_big_uns",
-    ["wh_main_sc_grn_savage_orcs"] = "wh_main_grn_inf_savage_orc_big_uns",
-    ["lololol wh_main_sc_ksl_kislev"] = "wh_main_emp_inf_halberdiers",
-    ["wh_main_sc_nor_norsca"] = "wh_main_nor_inf_chaos_marauders_0",
-    ["lololol wh_main_sc_teb_teb"] = "wh_main_emp_inf_halberdiers",
-    ["wh_main_sc_vmp_vampire_counts"] = "wh_main_vmp_inf_crypt_ghouls",
-    ["wh2_dlc09_sc_tmb_tomb_kings"] = "wh2_dlc09_tmb_inf_nehekhara_warriors_0",
-    ["wh2_main_sc_def_dark_elves"] = "wh2_main_def_inf_black_ark_corsairs_0",
-    ["wh2_main_sc_hef_high_elves"] = "wh2_main_hef_inf_spearmen_0",
-    ["wh2_main_sc_lzd_lizardmen"] = "wh2_main_lzd_inf_saurus_warriors_1",
-    ["wh2_main_sc_skv_skaven"]  = "wh2_main_skv_inf_stormvermin_0"
-}--:map<string, string>
+    ["wh_dlc03_sc_bst_beastmen"] = {"wh_dlc03_bst_inf_gor_herd_0", "wh_dlc03_bst_inf_ungor_raiders_0",  "wh_dlc03_bst_inf_ungor_spearmen_1", "wh_dlc03_bst_inf_gor_herd_0", "wh_dlc03_bst_inf_gor_herd_0"},
+    ["wh_dlc05_sc_wef_wood_elves"] = {"wh_dlc05_wef_inf_eternal_guard_1", "wh_dlc05_wef_inf_glade_guard_0", "wh_dlc05_wef_inf_dryads_0"},
+    ["wh_main_sc_brt_bretonnia"] = {"wh_main_brt_cav_knights_of_the_realm", "wh_dlc07_brt_inf_men_at_arms_2", "wh_main_brt_inf_peasant_bowmen", "wh_main_brt_cav_knights_of_the_realm"},
+    ["wh_main_sc_chs_chaos"] = {"wh_main_chs_inf_chaos_warriors_0", "wh_main_chs_cav_chaos_chariot", "wh_main_chs_inf_chaos_warriors_0", "wh_main_chs_inf_chaos_warriors_0", "wh_dlc01_chs_inf_forsaken_0"},
+    ["wh_main_sc_dwf_dwarfs"] = {"wh_main_dwf_inf_longbeards", "wh_main_dwf_inf_thunderers_0", "wh_main_dwf_inf_dwarf_warrior_0"},
+    ["wh_main_sc_emp_empire"] = {"wh_main_emp_inf_swordsmen", "wh_main_emp_inf_handgunners", "wh_main_emp_inf_halberdiers"},
+    ["wh_main_sc_grn_greenskins"] = {"wh_main_grn_inf_orc_big_uns", "wh_dlc06_grn_inf_nasty_skulkers_0", "wh_main_grn_inf_orc_arrer_boyz"},
+    ["wh_main_sc_grn_savage_orcs"] = {"wh_main_grn_inf_savage_orc_big_uns","wh_main_grn_inf_savage_orc_arrer_boyz"},
+    ["lololol wh_main_sc_ksl_kislev"] = {"wh_main_emp_inf_halberdiers"},
+    ["wh_main_sc_nor_norsca"] = {"wh_main_nor_inf_chaos_marauders_0", "wh_dlc08_nor_inf_marauder_hunters_1", "wh_main_nor_inf_chaos_marauders_0", "wh_dlc08_nor_inf_marauder_spearman_0", "wh_main_nor_cav_marauder_horsemen_0"},
+    ["lololol wh_main_sc_teb_teb"] = {"wh_main_emp_inf_halberdiers"},
+    ["wh_main_sc_vmp_vampire_counts"] = {"wh_main_vmp_inf_crypt_ghouls"}, 
+    ["wh2_dlc09_sc_tmb_tomb_kings"] = {"wh2_dlc09_tmb_inf_nehekhara_warriors_0", "wh2_dlc09_tmb_inf_skeleton_archers_0", "wh2_dlc09_tmb_veh_skeleton_archer_chariot_0", "wh2_dlc09_tmb_inf_nehekhara_warriors_0"},
+    ["wh2_main_sc_def_dark_elves"] = {"wh2_main_def_inf_black_ark_corsairs_0","wh2_main_def_inf_darkshards_0", "wh2_main_def_inf_dreadspears_0"},
+    ["wh2_main_sc_hef_high_elves"] = {"wh2_main_hef_inf_spearmen_0", "wh2_main_hef_inf_spearmen_0", "wh2_main_hef_inf_archers_1", "wh2_main_hef_cav_silver_helms_0", "wh2_main_hef_inf_lothern_sea_guard_1"},
+    ["wh2_main_sc_lzd_lizardmen"] = {"wh2_main_lzd_inf_saurus_warriors_1", "wh2_main_lzd_mon_kroxigors", "wh2_main_lzd_inf_saurus_warriors_1", "wh2_main_lzd_inf_skink_cohort_1"},
+    ["wh2_main_sc_skv_skaven"]  = {"wh2_main_skv_inf_stormvermin_0", "wh2_main_skv_inf_stormvermin_1", "wh2_main_skv_inf_gutter_runners_1"}
+}--:map<string, vector<string>>
 
 
 
@@ -65,8 +65,9 @@ local function limit_character(character, groupID, difference)
         for k = 1, #groups_list do
             if groups_list[k] == groupID then
                 cm:remove_unit_from_character(cm:char_lookup_str(character:cqi()), unit)
-                cm:grant_unit_to_character(cm:char_lookup_str(character:cqi()), subculture_default_units[character:faction():subculture()])
-                rm:log("removed unit ["..unit.."] and granted ["..subculture_default_units[character:faction():subculture()].."] as a replacement unit!")
+                local new_unit = subculture_default_units[character:faction():subculture()][cm:random_number(#subculture_default_units[character:faction():subculture()])]
+                cm:grant_unit_to_character(cm:char_lookup_str(character:cqi()), new_unit)
+                rm:log("removed unit ["..unit.."] and granted ["..new_unit.."] as a replacement unit!")
                 if rm:get_weight_for_unit(unit) >= diff then
                     rm:log("removed unit was sufficient!")
                     return
