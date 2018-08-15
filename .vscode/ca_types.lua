f945e313f180beb762aea673f3a84fc02d808994
DFlunchbreak <matte901@gmail.com>
2018-08-15 12:20:08 -0400

    added has skill method

--- .vscode/ca_types.lua
+++ .vscode/ca_types.lua
@@ -295,6 +295,8 @@
 --# assume CA_CHAR.has_military_force: method() --> boolean
 --# assume CA_CHAR.is_faction_leader: method() --> boolean
 --# assume CA_CHAR.family_member: method() --> CA_CHAR
+--# assume CA_CHAR.has_skill: method(skill: string) --> boolean
+
 
 -- CHARACTER LIST
 --# assume CA_CHAR_LIST.num_items: method() --> number
