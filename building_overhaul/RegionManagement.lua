9bc7b362fb26dfa52bd1fbf78cb12e30e2e69eee
DFlunchbreak <matte901@gmail.com>
2018-08-22 15:39:53 -0400

    created basic architecture

--- building_overhaul/RegionManagement.lua
+++ building_overhaul/RegionManagement.lua
@@ -5,14 +5,48 @@ local region_detail_manager = {} --# assume region_detail_manager: RDM
 
 --v function()
 function region_detail_manager.init()
+    local self = {}
+    setmetatable(self, {
+        __index = region_detail_manager
+    }) --# assume self: RDM
+
+    --religion and wealth impact
+    self._buildingReligionEffects = {} --:map<string, function(region: REGION_DETAIL)>
+    self._buildingWealthEffects = {} --:map<string, function(region:REGION_DETAIL)>
+    self._buildingUnitGenerationEffects = {} --:map<string, function(region: REGION_DETAIL)>
+
+    self._regions = {} --: map<string, REGION_DETAIL>
 
 
 end
 
+--v function(self: RDM, building: string) --> boolean
+function region_detail_manager.building_has_religion_effect(self, building)
+    return not not self._buildingReligionEffects[building]
+end
+--v function(self: RDM, building: string) --> boolean
+function region_detail_manager.building_has_wealth_effect(self, building)
+    return not not self._buildingWealthEffects[building]
+end
+--v function(self: RDM, building: string) --> boolean
+function region_detail_manager.building_has_unit_gen_effect(self, building)
+    return not not self._buildingUnitGenerationEffects[building]
+end
 
+--v function(self: RDM, building: string, effect: function(region: REGION_DETAIL))
+function region_detail_manager.add_building_religion_effect(self, building, effect)
+    self._buildingReligionEffects[building] = effect
+end
 
+--v function(self: RDM, building: string, effect: function(region: REGION_DETAIL))
+function region_detail_manager.add_building_wealth_effect(self, building, effect)
+    self._buildingWealthEffects[building] = effect
+end
 
-
+--v function(self: RDM, building: string, effect: function(region: REGION_DETAIL))
+function region_detail_manager.add_building_unit_gen_effect(self, building, effect)
+    self._buildingUnitGenerationEffects[building] = effect
+end
 
 
 
@@ -23,19 +57,22 @@ end
 local region_detail = {} --# assume region_detail: REGION_DETAIL
 
 
---v function(region_key: string, starting_wealth: number)
-function region_detail.new(region_key, starting_wealth)
+--v function(manager: RDM, region_key: string, starting_wealth: number)
+function region_detail.new(manager, region_key, starting_wealth)
     local self = {}
     setmetatable(self, {
         __index = region_detail
     })
     self._key = region_key
+    self._manager = manager
     self._buildings = {} --: map<string, boolean>
     --religion 
-    self._religions = {} --:map<string, number>
-    self._religionPoints = {} --:map<string, number>
+    self._activeReligions = {} --:map<string, number>
+    self._religionStrengths = {} --:map<string, number>
     --wealth
     self._wealth = starting_wealth
-    self._wealthCap = 0
+    self._wealthCap = 100 --:number
+    --unit generation
+    self._unitGeneration = {} --:map<string, number>
 
 end
