435405360063e3d4453919d0074ededbbdaf540d
DF <matte901@gmail.com>
2018-09-16 16:52:03 -0400

    ui outline

--- building_overhaul/export_helpers__province_management_ui.lua
+++ building_overhaul/export_helpers__province_management_ui.lua
@@ -2,8 +2,52 @@ pm = _G.pm
 local UIBUTTONNAME = "REGION_DETAILS_BUTTON"
 local UIPANELNAME = "REGION_DETAILS_PANEL"
 
+--v function(DetailsFrame: FRAME,fpd: FPD)
+local function PopulatePanel(DetailsFrame, fpd)
+    local fX, fY = DetailsFrame:Bounds()
+    local sX, sY = core:get_screen_resolution()
+    local FrameContainer = Container.new(FlowLayout.VERTICAL)
+    Util.centreComponentOnComponent(FrameContainer, DetailsFrame)    
+        local HorizontalHolder_1 = Container.new(FlowLayout.HORIZONTAL)
+            local TaxRateHolder = Container.new(FlowLayout.VERTICAL)
+
+            local ReligionHolder = Container.new(FlowLayout.VERTICAL)
+
+        HorizontalHolder_1:AddComponent(TaxRateHolder)
+        HorizontalHolder_1:AddGap(fX/10)
+        HorizontalHolder_1:AddComponent(ReligionHolder)
+
+        local HorizontalHolder_2 = Container.new(FlowLayout.VERTICAL)
+            local UnitProductionHolder = Container.new(FlowLayout.VERTICAL)
+
+            local WealthHolder = Container.new(FlowLayout.VERTICAL)
+
+        HorizontalHolder_2:AddComponent(UnitProductionHolder)
+        HorizontalHolder_2:AddGap(fX/10)
+        HorizontalHolder_2:AddComponent(WealthHolder)
+
+    FrameContainer:AddComponent(HorizontalHolder_1)
+    FrameContainer:AddGap(fY/6)
+    FrameContainer:AddComponent(HorizontalHolder_2)
+end
 
 
+
+--v function()
+local function CreatePanel()
+    local existingFrame = Util.getComponentWithName(UIPANELNAME)
+    if not not existingFrame then
+        --# assume existingFrame: FRAME
+        existingFrame:Delete()
+    end
+    local ProvinceDetailsFrame = Frame.new(UIPANELNAME)
+    
+    --resize frame to match UI of the settlement panel
+    --create a close button and move it to top right.
+    --set the panel title.
+    local currentFPD = pm._currentFPD
+    PopulatePanel(ProvinceDetailsFrame, currentFPD)
+end
 
 local function UIOnSettlementSelected()
     local ButtonParent
@@ -16,7 +60,7 @@ local function UIOnSettlementSelected()
         local ButtonParent = find_uicomponent(core:get_ui_root(), "layout") --, "info_panel_holder", "primary_info_panel_holder", "info_panel_background", "ProvinceInfoPopup", "parchment_banner"
         local DetailsButton = Button.new(UIBUTTONNAME, ButtonParent, "CIRCULAR", "ui/skins/default/icon_province_details.png")
         DetailsButton:MoveTo(8, 1016)
-        DetailsButton:RegisterForClick(function() end)
+        DetailsButton:RegisterForClick(function() CreatePanel() end)
     end
 end
 
