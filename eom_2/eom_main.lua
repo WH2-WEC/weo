b87a7ada13e775405c06eca0e93af970994ce44e
DrunkFlamingo <matte901@gmail.com>
2019-01-18 11:10:36 -0500

    started eom work

---
+++ eom_2/eom_main.lua
@@ -0,0 +1,8 @@
+--# assume global class EOM
+--# assume global class EOM_ELECTOR
+--# assume global class SIMPLE_EVENT
+--# assume global class PLOT_EVENT
+
+
+
+local elector = require("eom_2/eom_elector")
